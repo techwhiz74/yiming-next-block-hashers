a6f81b4e391790286d2f87dad9ba41143bfe2990
CoolD69 <parthasingharoy166@gmail.com>
2022-07-11 16:41:33 +0530

    tailWind Working fine

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
