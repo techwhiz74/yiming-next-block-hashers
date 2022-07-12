a46a78bd620f8f2adfe4274c917b0fdd10cbe084
yiming0704 <christoperw818@gmail.com>
2022-07-12 13:19:17 +0530

    Home page Ready

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <header class="text-gray-600 body-font">
-        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+    <div className="gradient">
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center ">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -13,33 +13,34 @@ const Navbar = () => {
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
               viewBox="0 0 24 24"
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span class="ml-3 text-xl">BLOCK-#ERS</span>
+            <span className="ml-3 text-2xl text-white font-bold">BLOCK-#ERS</span>
           </a>
-          <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            <a class="mr-5 hover:text-gray-900">First Link</a>
-            <a class="mr-5 hover:text-gray-900">Second Link</a>
-            <a class="mr-5 hover:text-gray-900">Third Link</a>
-            <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+          <nav className=" text-white  md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center  ">
+            <a className="mr-5  hover:text-gray-300">Home</a>
+            <a className="mr-5 hover:text-gray-300">About</a>
+            <a className="mr-5 hover:text-gray-300">Contact Us</a>
+            {/* <a className="mr-5 hover:text-gray-900"></a> */}
           </nav>
-          <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
-            <svg
+          <button className="px-6  bg-green-500 inline-flex items-center text-white border-0 py-1 focus:outline-none  rounded-3xl text-lg mt-4 md:mt-0">
+            Wallet
+            
+          </button>
+          <svg
               fill="none"
-              stroke="currentColor"
+              stroke="white"
               stroke-linecap="round"
               stroke-linejoin="round"
-              stroke-width="2"
-              class="w-4 h-4 ml-1"
+              stroke-width="4"
+              className="w-5 h-4 ml-1 "
               viewBox="0 0 24 24"
             >
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
-          </button>
         </div>
       </header>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,36 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import {Footer} from '../components/Footer'
+import NextNProgress from "nextjs-progressbar";
+
+import { useState, useEffect } from 'react'
+
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
-  return <>
 
+  const [progress, setProgress] = useState(0)
+  const router = useRouter()
+  useEffect(() => {
+
+    router.events.on('routeChangeStart', ()=>{
+      setProgress(40)
+    })
+    router.events.on('routeChangeComplete', ()=>{
+      setProgress(100)
+    })
+
+  }, [router.query])
+
+
+  return <>
+<NextNProgress
+ color='#f11946'
+ // color='#1f6feb'
+ progress={progress}
+ waitingTime={500}
+ onLoaderFinished={() => setProgress(0)}
+/>
  <Navbar/>
   <Component {...pageProps} />
 
