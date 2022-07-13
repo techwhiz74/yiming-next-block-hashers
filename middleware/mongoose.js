644cbfa6c6c89e5fedf2773f90f03b374226e5e6
CoolD69 <parthasingharoy166@gmail.com>
2022-07-13 21:09:27 +0530

    login & signup page ready

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -1,19 +1,24 @@
-//connected to MongoDB
+// //connected to MongoDB
 
 import mongoose from "mongoose";
+// const MONGO_URL = ""
 
 const connectDB = handler=> async (req,res)=>{
     if (mongoose.connections[0].readyState) {
         return handler(req,res)
     }
-   await mongoose.connect(process.env.MONGO_URL)
-   return handler(req,res)
+//  await mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log(`You are now connected to the MongoDB Atlas`)
+    return handler(req,res)
+})
+.catch((err)=>{
+    console.log(`Not Connected! ${err}`)
+})
 }
 
-export default connectDB;
-
-
-
+export default connectDB
 
 
 
