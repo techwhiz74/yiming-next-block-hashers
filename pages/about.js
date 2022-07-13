7342afccdae97c78e824bced7d723725923e24f6
CoolD69 <parthasingharoy166@gmail.com>
2022-07-13 23:34:48 +0530

    Reday

--- pages/about.js
+++ pages/about.js
@@ -5,67 +5,41 @@ import Image from 'next/image'
 const about = () => {
     
     return (
-        <div className='bg-gray-100 flex bg-02'>
+        <div className='bg-gray-100 flex '>
             
 
 
 	
 		<div className="container mx-auto ">
-			<div className="flex justify-center px-6 my-12">
+			<div className="flex justify-center px-6 my-12 ">
 			
-				<div className="w-full xl:w-3/4 lg:w-11/12 flex">
+				<div className="w-full xl:w-3/4 lg:w-11/12 flex shadow-lg">
 					
 					<div
-						className="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"
+						className="w-full bg-white  hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"
 						
 					>
-                      
+                      <Image src={'/Block_Hashers.png'} width={720} height={720} className="rounded"></Image>
 
                     </div>
 			
 					<div className="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
 						<div className="px-8 mb-4 text-center">
-							<h3 className="pt-4 mb-2 text-2xl">Forgot Your Password?</h3>
+							<h3 className="pt-4 mb-2 font-bold text-3xl">About Us</h3>
 							<p className="mb-4 text-sm text-gray-700">
-								We get it, stuff happens. Just enter your email address below and we'll send you a
-								link to reset your password!
+								{/* about us write up */}
 							</p>
 						</div>
 						<form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
 							<div className="mb-4">
+								We the Students of kalyani Government Engineering College
 								
-								<input
-									className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-									id="email"
-									type="email"
-									placeholder="Enter Email Address..."
-								/>
 							</div>
 							<div className="mb-6 text-center">
-								<button
-									className="w-full px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:shadow-outline"
-									type="button"
-								>
-									Reset Password
-								</button>
+								
 							</div>
 							<hr className="mb-6 border-t" />
-							<div className="text-center">
-								<a
-									className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-									href="./register.html"
-								>
-									Create an Account!
-								</a>
-							</div>
-							<div className="text-center">
-								<a
-									className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-									href="./index.html"
-								>
-									Already have an account? Login!
-								</a>
-							</div>
+							
 						</form>
 					</div>
 				</div>
